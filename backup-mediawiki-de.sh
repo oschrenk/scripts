43228b1ef30b6543a78b7b19801c036c940259f4
Oliver Schrenk <oliver.schrenk@gmx.net>
2009-11-27 02:21:38 +0100

    Added backup-mediawiki alternative for german installations

---
+++ backup-mediawiki-de.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+# Inspired by blogpost from http://www-public.it-sudparis.eu/~berger_o/weblog/2008/05/30/offline-backup-mediawiki-with-httrack/
+
+site=www.host.tld
+topurl=http://$site
+
+backupdir=~/Downloads/mirrors/$site
+
+httrack -%i -w $topurl/Spezial:Alle_Seiten \
+-O "$backupdir" -%P -N0 -s0 -p7 -S -a -K0 -%k -A25000 \
+-F "Mozilla/4.5 (compatible; HTTrack 3.0x; Windows 98)" \
+-%s -x -%x -%u \
+"-$site/Spezial:*" \
+"-$site/index.php?title=Spezial:*" \
+"+$site/Spezial:Letzte_Änderungen" \
+"+*.css" \
+"-$site/index.php?title=*&oldid=*" \
+"-$site/index.php?title=*&action=edit" \
+"-$site/index.php?title=*&curid=*" \
+"+$site/index.php?title=*&action=history" \
+"-$site/index.php?title=*&action=history&*" \
+"-$site/index.php?title=*&curid=*&action=history*" \
+"-$site/index.php?title=*&limit=*&action=history"
+
+for page in $(grep "link updated: $site/index.php/" $backupdir/hts-log.txt | sed "s,^.*link updated: $site/index.php/,," | sed ’s/ ->.*//’ | grep -v Spezial:)
+do
+wget -nv -O $backupdir/$site/index.php/${page}_raw.txt "$topurl/index.php?index=$page&action=raw"
+done
