1530179167b611b5f0767165c959878c5c362b28
Oliver Schrenk <oliver.schrenk@gmx.net>
2009-11-29 19:10:45 +0100

    Added comments about used parameters

--- backup-mediawiki.sh
+++ backup-mediawiki.sh
@@ -2,6 +2,24 @@
 
 # Inspired by blogpost from http://www-public.it-sudparis.eu/~berger_o/weblog/2008/05/30/offline-backup-mediawiki-with-httrack/
 
+# %i		???
+# -w		mirror web sites (--mirror)
+# -O 		backup directory
+# -%P 		extended parsing, attempt to parse all links, even in unknown tags or Javascript (%P0 don't use) (--extended-parsing[=N])
+# -N0 		Saves files like in site Site-structure (default)
+# -s0 		follow robots.txt and meta robots tags (0=never,1=sometimes,* 2=always) (--robots[=N])
+# -p7 		Expert options, priority mode: 7 > get html files before, then treat other files
+# -S 		Expert option, stay on the same directory
+# -a 		Expert option, stay on the same address
+# -K0 		keep original links (e.g. http://www.adr/link) (K0 *relative link, K absolute links, K3 absolute URI links) (--keep-links[=N]
+# -%k 		???
+# -A25000 	maximum transfer rate in bytes/seconds (1000=1kb/s max) (--max-rate[=N])
+# -F 		user-agent field (-F "user-agent name") (--user-agent )
+# -%s		update hacks: various hacks to limit re-transfers when updating (identical size, bogus response..) (--updatehack)
+# -x 		Build option, replace external html links by error pages
+# -%x 		Build option, do not include any password for external password protected websites (%x0 include) (--no-passwords)
+# -%u		???
+
 site=wiki.host.tld
 topurl=http://$site
 
