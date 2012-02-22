5fbb7791d4dcd2a3ab3a21d89f0d46071c177084
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-02-22 18:51:08 +0100

    - add repository and revision number as parameter - progress output - better grep for revision number

--- svnauthors.sh
+++ svnauthors.sh
@@ -3,7 +3,9 @@
 # Run this inside an subversion checkout. It outputs a template for the
 # svn.authorsfile to the console, so you just need to paste it into a 
 # document, and fill in the names and email address for your authors.
-authors=$(svn log -q | grep -e '^r' | awk 'BEGIN { FS = "|" } ; { print $2 }' | sort | uniq)
+#
+# Usage example: svnauthors.sh http://company.com/repo
+authors=$(svn log -q $@ | grep -e '^r[0-9]' | awk 'BEGIN { FS = "|" } ; { print $2 } $1 ~ /r([0-9]+000)/ { print "fetched revision " substr($1, 2) > "/dev/stderr" }' | sort | uniq) 
 for author in ${authors}; do
   echo "${author} = NAME <USER@DOMAIN>";
 done
