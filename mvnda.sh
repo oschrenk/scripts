77efc3b6d439b69f0c5afedf817a08e8d5d985fc
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-08-29 13:57:57 +0200

    Bumo archetype version to 1.0.1

--- mvnda.sh
+++ mvnda.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn archetype:generate -DarchetypeGroupId=com.oschrenk.maven -DarchetypeArtifactId=default-archetype -DarchetypeVersion=1.0
+mvn archetype:generate -DarchetypeGroupId=com.oschrenk.maven -DarchetypeArtifactId=default-archetype -DarchetypeVersion=1.0.1
