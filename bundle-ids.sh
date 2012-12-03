2101949c5872503f1fb094f3608dabc58ae44d43
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-12-03 20:15:57 +0100

    Print OSX bundle ids

---
+++ bundle-ids.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+names="
+App Store
+Automator
+Calculator
+Calendar
+Cog
+Contacts
+Dashboard
+Dictionary
+Gimp
+iPhoto
+iTunes
+LibreOffice
+Mail
+MPlayerX
+Preview
+Sublime Text 2
+VLC
+"
+
+tmp=/tmp/$(uuidgen)
+mdfind -onlyin / 'kMDItemContentType==com.apple.application-bundle' > $tmp
+echo "$names" | sed 's/^/\//g;s/$/.app/g;s/ /\\ /g' | xargs -L 1 -J '{}' grep -m 1 -i -F '{}' $tmp | sed 's/ /\\ /g' | xargs mdls -name kMDItemCFBundleIdentifier | sed 's/.*= "\(.*\)"$/\1/'
+rm $tmp
