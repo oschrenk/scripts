729f81fd8b196572f28b20077f0420e9d85f3286
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-12-17 11:41:35 +0100

    Pull changes from upstream
    https://raw.github.com/12k/osx-proxy/25bc65e26041b8029ba167bf9dbd642872229a9d/proxy

--- tunnel.sh
+++ tunnel.sh
@@ -1,30 +1,103 @@
 # !/bin/sh
 # SSH SOCKS proxy script for Mac OS X
 
-local_port=`jot -r 1 2000 65000`
+# Set PROXY_USER and PROXY_HOST
+#
+# If choosing not to set credentials as Environmental Variables, replace the values below for $PROXY_USER and $PROXY_HOST with genuine credentials.
+# ie
+# remoteuser='user'
+# remoteproxy='example.com'
 
-if [ $1 == "on" ]; then
-  echo "Listening on localhost:$local_port. Modifying network settings.."
-  sudo networksetup -setsocksfirewallproxy Wi-Fi 127.0.0.1 $local_port off
+# Get the min and max system-available ports.
+lowerport=`sysctl net.inet.ip.portrange.first | cut -d " " -f 2`
+upperport=`sysctl net.inet.ip.portrange.last | cut -d " " -f 2`
+localport=`jot -r 1 $lowerport $upperport`
+
+remoteuser=$PROXY_USER
+remoteproxy=$PROXY_HOST
+remoteport="22"
+
+usage(){
+echo ""
+echo "Usage: proxy [on|off|killall|shutdown|no_args]"
+echo "Proxy is a proxy settings toggle script for OSX"
+echo "Proxy initiates an SSH tunnel and then enables a Socks proxy"
+}
+
+unknown_input(){
+echo "Unknown input, try again with different term"
+}
+
+get_proxy_state(){
+  result=`networksetup -getsocksfirewallproxy Wi-Fi | head -1 | cut -d ' ' -f2`
+  echo $result
+}
+
+proxy_on(){
+echo "Listening on localhost:$localport. Modifying network settings.."
+  sudo networksetup -setsocksfirewallproxy Wi-Fi 127.0.0.1 $localport off
   echo "Starting SSH session. Will run in background for 1 day."
-  ssh -f tunnel -N -D localhost:$local_port sleep 1d
+  ssh -f tunnel -N -D localhost:$localport sleep 1d
 
-  remote_ip=`curl -s -S --socks5 127.0.0.1:$local_port http://automation.whatismyip.com/n09230945.asp`
+  remote_ip=`curl -s -S --socks5 127.0.0.1:$localport http://automation.whatismyip.com/n09230945.asp`
   echo "Your remote ip is $remote_ip"
-  echo "http_proxy has NOT been set."
-fi
+  echo "The http_proxy for the terminal has NOT been set."
+}
 
-if [ $1 == "off" ]; then
+proxy_off(){
   echo "Disabling proxy in network settings."
   sudo networksetup -setsocksfirewallproxystate Wi-Fi off
   echo "Done!"
-fi
+}
 
-if [ $1 == "killall" ]; then
+kill_all(){
   pids=`ps -A | grep "ssh -f" | grep "sleep" | awk '{print $1}'`
   echo "Killing all running proxy connections."
   for pid in $pids
     do
   `kill -9 $pid`
   done
-fi
+}
+
+shutdown(){
+  proxy_off
+  kill_all
+  echo "Proxy shutdown complete!"
+}
+
+toggle_state(){
+  # result=`get_proxy_state`
+  echo $(get_proxy_state) | grep -i 'y' >> /dev/null
+  if [[ $? =~ 0 ]]; then
+    shutdown
+    echo "OFF"
+  else
+    proxy_on
+    echo "ON"
+  fi
+}
+
+case $1 in
+"on")
+  proxy_on
+  ;;
+"off")
+  proxy_off
+  ;;
+"killall")
+  kill_all
+  ;;
+"shutdown")
+  shutdown
+  ;;
+"usage")
+  usage
+  ;;
+"")
+  toggle_state
+  ;;
+*)
+  unknown_input
+  usage
+  ;;
+esac
