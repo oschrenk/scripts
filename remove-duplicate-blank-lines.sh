14b4440686dbce26fe96568dc90a4f9c76a057ef
Oliver Schrenk <oliver.schrenk@gmail.com>
2013-11-02 13:56:50 +0100

    Handle whitespace in file names

--- remove-duplicate-blank-lines.sh
+++ remove-duplicate-blank-lines.sh
@@ -2,4 +2,4 @@
 
 # Reduces two or more consecutive blank lines to a single blank line
 
-awk '/^$/{ if (! blank++) print; next } { blank=0; print }' $1 > $1.old; rm $1; mv $1.old $1;
+awk '/^$/{ if (! blank++) print; next } { blank=0; print }' "$1" > "$1.old"; rm "$1"; mv "$1.old" "$1";
